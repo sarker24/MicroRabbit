67e3753fbeb20570c8720efe5fe401c5256d9399
Murad <sarker24@gmail.com>
2019-12-15 23:35:51 +0100

    Add DBContext

---
+++ MicroRabbit/MicroRabbit.Cataring.Data/Context/CataringDBContext.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Cataring.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Cataring.Data.Context
+{
+   public class CataringDBContext: DbContext
+    {
+        public CataringDBContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Cataring.Domain/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Cataring.Domain.Models
+{
+   public class Order
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EventLocation { get; set; }
+        public string Address { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
